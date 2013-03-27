0ae79644287eef99b0760e48c7dd712b4f5b7570
Jeremy Heiler <jeremyheiler@gmail.com>
2013-03-27 19:08:54 -0400

    Bump Clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/jeremyheiler/xenopath"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.0"]]}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
