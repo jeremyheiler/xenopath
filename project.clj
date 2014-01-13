4490d0b1e54294cbd4875edf8fc6febd7d051ed1
Jeremy Heiler <jeremyheiler@gmail.com>
2014-01-13 18:09:08 -0500

    Prepare for 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xenopath "0.1.1"
+(defproject xenopath "0.1.2"
   :description "XPath for Clojure"
   :url "https://github.com/jeremyheiler/xenopath"
   :license {:name "Eclipse Public License"
